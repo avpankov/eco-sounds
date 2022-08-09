91c2010bcca77e476d6a3ba63dbf58d759bef8bf
Artyom Pankov <apankov4696@gmail.com>
2022-08-09 21:24:55 +0300

    feat: add initial sound

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-import bootstrap from 'bootstrap';
+let navList = document.querySelector('.nav__list');
+let audio = document.createElement('audio');
+document.body.appendChild(audio);
+
+// navList.addEventListener('click', (e) => {
+//     console.log(e.target);
+// })
+
+let play = document.querySelector('.play');
+let pause = document.querySelector('.pause');
+
+play.addEventListener('click', () => {
+    play.style.display = 'none';
+    pause.style.display = 'block';    
+    playAudio('forest');
+});
+
+pause.addEventListener('click', () => {
+    play.style.display = 'block';
+    pause.style.display = 'none'; 
+    audio.pause();
+});
+
+
+function playAudio(bird) {
+    audio.src = `./src/assets/audio/${bird}.mp3`;
+    audio.autoplay = true;
+    audio.currentTime = 0;
+    audio.play();
+}
+
